ef5deb9b104a1f098f1f827a8317bd6d19f40a6f
YoshikazuHosono <yyhhwork@gmail.com>
2021-12-30 12:34:02 +0900

    save

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,41 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.receiver.SampleReceiver;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
-
-    @Value("${app-config.rabbitmq.exchange-name}")
-    private String topicExchangeName;
-
-    @Value("${app-config.rabbitmq.queue-name}")
-    private String queueName;
-
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
-    }
-
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(topicExchangeName);
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("#");
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/com/example/demo/config/RabbitmqConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+    @Value("${app-config.rabbitmq.exchange-name}")
+    private String topicExchangeName;
+
+    @Value("${app-config.rabbitmq.queue-name}")
+    private String queueName;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("#");
+    }
+}
